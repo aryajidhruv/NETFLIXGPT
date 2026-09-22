e4882bee2ee19b017bf4c15e0732e6ee143f42a5
aryajidemo <aryajiidhruv@gmail.com>
2026-09-22 13:12:06 +0530

    Fix remaining eslint warnings: useMovieTrailer deps and firebase app usage

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,30 +1,28 @@
-import  { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { API_OPTION } from '../utils/constants';
 import { addTrailerVideo } from '../utils/moviesSlice';
 
 const useMovieTrailer = (movieId) => {
-      const dispatch =  useDispatch();
-     
-  
+      const dispatch = useDispatch();
+
       const getMovieVideo = async () => {
           const data = await fetch(
-              "https://api.themoviedb.org/3/movie/"+ movieId +"/videos?language=en-US",
+              "https://api.themoviedb.org/3/movie/" + movieId + "/videos?language=en-US",
               API_OPTION
           )
-  
+
           const json = await data.json();
-          console.log(json)
-  
+
           const filterData = json.results.filter((video) => video.type === "Trailer")
           const trailer = filterData.length ? filterData[0] : json.results[0];
-  
-  
+
           dispatch(addTrailerVideo(trailer))
       }
-  
+
       useEffect(() => {
           getMovieVideo()
+          // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [movieId])
 }
 
