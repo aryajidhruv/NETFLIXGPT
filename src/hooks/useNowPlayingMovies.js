abe11e572364a33771ebae29d9512ecf8225d6ac
aryajidemo <aryajiidhruv@gmail.com>
2026-09-22 13:04:42 +0530

    Fix eslint exhaustive-deps warning in useNowPlayingMovies

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,26 +1,26 @@
-
-import  { useEffect } from 'react'
+import { useEffect } from 'react'
 
 import { API_OPTION } from '../utils/constants'
 import { useDispatch } from 'react-redux'
 import { addNowPlayingMovies } from '../utils/moviesSlice'
+
 const useNowPlayingMovies = () => {
 
   //fetching the data and updating the store
-  
+
   const dispatch = useDispatch();
-  const getNowPlayingMovies = async()=>{
-  const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTION)
- 
-  const json =  await data.json();
-  console.log(json.results);
-  dispatch(addNowPlayingMovies(json.results))
+  const getNowPlayingMovies = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTION)
+
+    const json = await data.json();
+    dispatch(addNowPlayingMovies(json.results))
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getNowPlayingMovies()
-  },[])
- 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
 }
 
 export default useNowPlayingMovies;
